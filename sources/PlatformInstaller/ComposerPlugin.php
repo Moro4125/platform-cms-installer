665e8adf59eaec33618f2e9996f789280f396c71
Andrey Morozkin <andrey.dmitrievich@gmail.com>
2015-10-14 17:22:01 +0300

    Fix: composer.json - classmap

--- sources/PlatformInstaller/ComposerPlugin.php
+++ sources/PlatformInstaller/ComposerPlugin.php
@@ -1,19 +1,24 @@
 <?php
+/**
+ * Class PlatformInstallerPlugin
+ */
+namespace Moro\Platform\Installer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 /**
  * Class PlatformInstallerPlugin
+ * @package Moro\Platform\Installer
  */
-class PlatformInstallerPlugin implements PluginInterface
+class ComposerPlugin implements PluginInterface
 {
     /**
      * Called when the plugin is activated.
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $installer = new ComponentInstaller\Installer($io, $composer);
+        $installer = new \ComponentInstaller\Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
 }
